f8fbdd0ea50e036c7810100c19b3dbf5bbc072c7
HarbenSoft <benharkh@gmail.com>
2021-06-14 14:07:44 +0100

    add professeurs models

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'blog', 'contact', 'home', 'accounts','professeurs',
 ]
+
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
